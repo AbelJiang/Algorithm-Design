555674610550425e2de23e749b2a1193ddd2a05f
Wen Jiang <abeljiang@hotmail.com>
2017-10-23 11:20:21 -0700

    Fininsh NH

--- NumberHoles.java
+++ NumberHoles.java
@@ -1,6 +1,27 @@
+import java.util.HashSet;
+
 public class NumberHoles{
     public static void main(String[] args){
-        int input=Integer.parseInt(args[0]);
-        System.out.println(input);
+        System.out.println("Holes in this string: "+ countHoles(args[0]));
+    }
+
+    public static int countHoles(String input){
+        boolean[] oneHole=new boolean[10];
+        oneHole[0]=true;
+        oneHole[4]=true;
+        oneHole[6]=true;
+        oneHole[8]=true;
+        oneHole[9]=true;
+        int count=0;
+        for(int i=0;i<input.length();i++){
+            boolean x=oneHole[input.charAt(i)-'0'];
+            if(x==true){
+                count++;
+                if(input.charAt(i)=='8'){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
